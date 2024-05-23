e416a16752486d442fbb1fea2918c747be411586
Henrique Damasceno <henriquim25@yahoo.com.br>
2024-05-23 15:16:07 -0300

    feat: users creation implementation with jwt services

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,26 +1,35 @@
-import { Injectable } from '@nestjs/common';
-import { CreateAuthDto } from './dto/create-auth.dto';
-import { UpdateAuthDto } from './dto/update-auth.dto';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { UserService } from 'src/user/user.service';
+import * as bcrypt from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
+import { IUser } from './types/types';
 
 @Injectable()
 export class AuthService {
-  create(createAuthDto: CreateAuthDto) {
-    return 'This action adds a new auth';
+  constructor(
+    private readonly userService: UserService,
+    private readonly jwtService: JwtService,
+  ) {}
+  async validateUser(email: string, passsword: string) {
+    const user = await this.userService.findOne(email);
+    const passswordIsMatch = await bcrypt.compare(passsword, user.password);
+    if (user && passswordIsMatch) {
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      const { id, password, ...result } = user;
+      return result;
+    }
+    throw new UnauthorizedException('User or password are incorrect!');
   }
 
-  findAll() {
-    return `This action returns all auth`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} auth`;
-  }
-
-  update(id: number, updateAuthDto: UpdateAuthDto) {
-    return `This action updates a #${id} auth`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} auth`;
+  async login(user: IUser) {
+    const { id, email } = user;
+    return {
+      id,
+      email,
+      token: this.jwtService.sign({
+        id: user.id,
+        email: user.email,
+      }),
+    };
   }
 }
